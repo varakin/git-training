63541b34b964397c390a2367e9581e0c8d5a4923
varakin <varakin_ivan@yahoo.com>
2026-06-18 06:23:05 +0300

    add gitignore, fix first.py

--- first.py
+++ first.py
@@ -1,26 +1,6 @@
 import random
 
 list = ['собрать ковдозеро в одну папку',
-    'купить антивозгарайку',
-    'qwe',
-    'asd',
-    'zxc',
-    'rty',
-    'fgh',
-    'zxcvzxcvzxvczxcvcxzv',
-    'zxcvzxcvxzcvzxcvzxcvzxcvzxcvzxcvzxcv',
-        'asdfasdfdsfdsfsdafsdafsdafsdafsdafsdafdasfsdafsdafsadfsdafdsafsdafsdafsdaf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'asdklfjsdajfsdajfkldsajfkladjsfjsdakl;fjdsakl;fjsdkla;jf',
-    'vbn']
-
+    'купить антивозгарайку', 'qwerty']
 rez = random.choice(list)
 print(rez.upper())
